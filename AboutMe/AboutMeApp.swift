747fc17253c2ae83bbbcbb6965cd1dbb9b3aab52
peter <apppeterpan@gmail.com>
2023-01-20 21:13:01 +0800

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Peter Pan on 2023/1/20.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
